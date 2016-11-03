932ff898c9e3e47e05857305f78fe016c46f1a30
khlee2016 <khlee@seoultech.ac.kr>
2016-11-03 15:38:33 +0900

    Add files via upload

---
+++ khlee2016/khlee2016/hello.java
@@ -0,0 +1,8 @@
+public class hello {
+      public static void main(String[] argv) {
+            System.out.println("Hello Java");
+            for (int i=0 ; i<argv.length ; i++) {
+                 System.out.println(argv[i]);
+            }
+      }
+}
